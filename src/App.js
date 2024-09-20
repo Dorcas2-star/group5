6b1e406e1d048ef535ef4d26e072de24f03dbf3e
dorcas2-star <dodobyukusenge889847@gmail.com>
2024-09-20 12:54:59 +0200

    changing my code updatd

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './home';
 import myVideo from '../src/assets/video/nn.mp4';
-import kigali from "../src/assets/kgl.jpg"
+
 <Home />
 function App() {
   return (
@@ -12,7 +12,6 @@ function App() {
           <div class="logo">
             <a href='#' ><img src ="https://www.2exhibitions.com/wp-content/uploads/2022/07/kigali-convention-centre-kigali-rwanda.png"></img></a>
           </div>
-                  <img src={kigali} alt="" srcset="" />
                <button><a href="#">Home</a></button>  
                <button><a href="#">About</a></button> 
                <button><a href="#">Services</a></button> 
@@ -21,9 +20,7 @@ function App() {
               </header>
               <div id="home">
 
-
-
-  <video autoplay loop muted>
+  <video  controls>
     <source src={myVideo} type="video/mp4" />
     Your browser does not support the video tag.
   </video>
