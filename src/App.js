cbfcb694e5d69fa6e661d0065219e66eb6162c08
dorcas2-star <dodobyukusenge889847@gmail.com>
2024-09-26 11:35:04 +0200

    hello kayove

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import logo from './logo.svg';
+import Image from "./assets/Screenshot 2024-09-26 092408.png";
 import './App.css';
 import Home from './home';
-import myVideo from '../src/assets/video/nn.mp4';
+import Logo from './assets/equity-bank-logo.jpg'
+
 
 <Home />
 function App() {
   return (
     <>
-    <header>
-      <nav>
-          <div class="logo">
-            <a href='#' ><img src ="https://www.2exhibitions.com/wp-content/uploads/2022/07/kigali-convention-centre-kigali-rwanda.png"></img></a>
-          </div>
-          
-               <a href="#">Home</a>  
-               <a href="#">About</a> 
-               <a href="#">Services</a> 
-               <a href="#">Contact</a> 
-                  </nav>
-              </header>
-              <div id="home">
-
-  <video  controls>
-    <source src={myVideo} type="video/mp4" />
-    Your browser does not support the video tag.
-  </video>
-
- 
-  <div class="bg-overlay">
-    <p>YES WE CAN</p>
-  </div>
-</div>
-      </>
+    <div class='top'>
+      <select>
+        <option value="COUNTRY">COUNTRY</option>
+        <option value="KEN">KEN</option>
+        <option value="UGD">UGD</option>
+        <option value="TZN">TZN</option>
+        <option value="RWD">RWD</option>
+        <option value="SS">SS</option>
+        <option value="DRC">DRC</option>
+      </select>
+      <button>SEARCH</button>
+    </div>
+    <hr />
+    <div class="Menu">
+       <a><img  src={Logo} /></a>
+       <div class="min-menu">
+        <a href="#">About Equity</a>
+        <a href="#"> Investor Relations</a>
+        <a href="#"> Equity Group Foundations</a>
+        <a href="#"> Finserve</a>
+        <a href="#"> Newsroom</a>
+        <a href="#"> Knowledge Resource</a>
+        <a href="#" id="Eezy">Eazy</a>
+       </div>
+    </div>
+    <div class="button1">
+      <a href="#"><img src={Image} /></a>
+    </div>
+    </>
   );
 }
 
